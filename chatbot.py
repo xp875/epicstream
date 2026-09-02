047af3dfe0e440527c1b3752a5e217b23e005885
xp875 <liuxiuping1@gmail.com>
2026-09-02 15:51:49 +0800

    Rename main.py to chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+instructions = "Answer concisely. You like to say the words 'joever', 'joevering', 'joevered; at random places a lot."
+
+st.write("## OpenJI chad")
+
+with st.form("prompt_form"):
+    prompt = st.text_input  ("Enter your prompt:")
+    submit_button = st.form_submit_button(label="Submit")
+
+with st.container():
+    if submit_button:
+        st.write("Thinking...")
+        print(f"Prompt: {prompt}")
+        response = client.responses.parse(
+            model = "gpt-5.6-luna",
+            instructions = instructions,
+            reasoning={
+                "effort": "low"  # Choose from "low", "medium", or "high"
+            },
+            # Ensure "tools" either omits {"type": "web_search"} or is completely empty
+            tools=[], 
+            input = prompt
+        )
+
+        st.balloons()
+        st.write("Response:")
+        st.write(response.output_text)
+        estimated_cost = response.usage.input_tokens * 20 + response.usage.output_tokens * 120
+        estimated_cost /= 1e6
+        st.write(f"Estimated cost: {estimated_cost:.2} cents")
+
