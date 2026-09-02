a52eb6db8f73fdbcac57a06727db5c3da9cd23d9
xp875 <liuxiuping1@gmail.com>
2026-09-03 00:40:11 +0800

    Create multi page app: move pages into folder, create landing page

--- dataninja.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import streamlit as st
-
-ramen_ratings = pd.read_csv("ramen-ratings.csv")
-st.write(ramen_ratings)
--- pages/1_Learn.py
+++ pages/1_Learn.py
@@ -2,17 +2,17 @@ import streamlit as st
 from openai import OpenAI
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-instructions = "Answer concisely. You like to say the words 'joever', 'joevering', 'joevered; at random places a lot."
+instructions = "You are a teacher from Duopingo who helps the user learn anything. Generate a readable and engaging lesson based on the user's input. "
 
-st.write("## OpenJI chad")
+st.write("# Duopingo: learn anything")
+st.write("## Daily Lesson, powerd by OpenJI chad")
 
 with st.form("prompt_form"):
-    prompt = st.text_input  ("Enter your prompt:")
+    prompt = st.text_area("Describe what you want to learn:")
     submit_button = st.form_submit_button(label="Submit")
 
-with st.container():
-    if submit_button:
-        st.write("Thinking...")
+if submit_button:
+    with st.spinner("Generating lesson..."):
         print(f"Prompt: {prompt}")
         response = client.responses.parse(
             model = "gpt-5.6-luna",
@@ -25,10 +25,9 @@ with st.container():
             input = prompt
         )
 
-        st.balloons()
-        st.write("Response:")
-        st.write(response.output_text)
-        estimated_cost = response.usage.input_tokens * 20 + response.usage.output_tokens * 120
-        estimated_cost /= 1e6
-        st.write(f"Estimated cost: {estimated_cost:.2} cents")
+
+    st.write(response.output_text)
+    estimated_cost = response.usage.input_tokens * 20 + response.usage.output_tokens * 120
+    estimated_cost /= 1e6
+    st.write(f"Estimated cost: {estimated_cost:.2} cents")
 
